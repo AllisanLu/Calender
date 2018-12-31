334301d7f824b472c643b5ead417152660bca4e8
Ethan Neece <ethan.neece1@gmail.com>
2018-12-31 16:37:16 -0500

    starting

--- src/Driver.java
+++ src/Driver.java
@@ -1,2 +1,18 @@
-public class Driver {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Driver extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(new StackPane(), 1980, 1080));
+        primaryStage.show();
+    }
 }
