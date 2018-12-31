47ac622330275a2053d04530e4dc7178ea90dfac
Allison <alcp2610@gmail.com>
2018-12-31 17:01:02 -0500

    YAS

--- src/Driver.java
+++ src/Driver.java
@@ -11,7 +11,7 @@ public class Driver extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("DIS DA TITLE BC ALLI IS SO SMART");
         primaryStage.setScene(new Scene(new StackPane(), 1980, 1080));
         primaryStage.show();
     }
