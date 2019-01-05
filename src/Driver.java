3e65526736f87509d8399b8241dc0e8003e75b15
Allison <alcp2610@gmail.com>
2019-01-04 20:46:27 -0500

    Doop started on a day

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,8 @@
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
@@ -16,9 +18,20 @@ public class Driver extends Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Life Plans");
         BorderPane root = new BorderPane();
+
         GridPane calender = new GridPane();
         calender.setAlignment(Pos.CENTER);
         root.setCenter(calender);
+        Label labelDay = new Label("day 1          ");
+
+        labelDay.setStyle("-fx-border-color: #7f9ed1; -fx-font: 18;");
+        calender.add(labelDay, 0, 0, 1, 1);
+
+        Button add = new Button("+");
+        add.setStyle("-fx-base: #7f9ed1");
+        calender.add(add, 1, 0, 5, 3);
+
+
         primaryStage.setScene(new Scene(root, 1980,1080));
         primaryStage.show();
     }
