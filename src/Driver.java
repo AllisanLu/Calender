d7ad742f9e49a39a1367e364feee1aaa48dd4ecf
Allison <alcp2610@gmail.com>
2019-01-04 22:38:31 -0500

    better formating

--- src/Driver.java
+++ src/Driver.java
@@ -22,17 +22,24 @@ public class Driver extends Application {
         GridPane calender = new GridPane();
         calender.setAlignment(Pos.CENTER);
         root.setCenter(calender);
-        Label labelDay = new Label("day 1          ");
 
-        labelDay.setStyle("-fx-border-color: #7f9ed1; -fx-font: 18;");
+        addDay(primaryStage, calender);
+
+        primaryStage.setScene(new Scene(root, 1980,1080));
+        primaryStage.show();
+    }
+
+    public void addDay(Stage primaryStage, GridPane calender) throws Exception {
+        Label labelDay = new Label("Day 1          ");
+        labelDay.setStyle("-fx-border-color: #7f9ed1; -fx-font-size: 16;");
         calender.add(labelDay, 0, 0, 1, 1);
 
         Button add = new Button("+");
         add.setStyle("-fx-base: #7f9ed1");
         calender.add(add, 1, 0, 5, 3);
 
-
-        primaryStage.setScene(new Scene(root, 1980,1080));
-        primaryStage.show();
+        Label labelEvents = new Label("Eat food                   \n");
+        labelEvents.setStyle("-fx-border-color: #825a96;");
+        calender.add(labelEvents, 0, 1, 2, 3);
     }
 }
