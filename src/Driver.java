db22b0c283fcfeea440d689e0160cd3af2efd17f
Allison <alcp2610@gmail.com>
2019-01-05 19:16:25 -0500

    added something

--- src/Driver.java
+++ src/Driver.java
@@ -3,10 +3,13 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javax.swing.*;
 
 public class Driver extends Application {
 
@@ -30,16 +33,18 @@ public class Driver extends Application {
     }
 
     public void addDay(Stage primaryStage, GridPane calender) throws Exception {
-        Label labelDay = new Label("Day 1          ");
-        labelDay.setStyle("-fx-border-color: #7f9ed1; -fx-font-size: 16;");
+        ImageView dayDesign = new ImageView(new Image(Driver.class.getResourceAsStream("Day Design.png")));
+        calender.add(dayDesign, 0, 0, 2, 4);
+
+        Label labelDay = new Label(" Day 1");
+        labelDay.setStyle("-fx-font-size: 16;");
         calender.add(labelDay, 0, 0, 1, 1);
 
         Button add = new Button("+");
         add.setStyle("-fx-base: #7f9ed1");
-        calender.add(add, 1, 0, 5, 3);
+        calender.add(add, 5, 0, 5, 3);
 
-        Label labelEvents = new Label("Eat food                   \n");
-        labelEvents.setStyle("-fx-border-color: #825a96;");
+        Label labelEvents = new Label("  -Eat food"); //where to add time?
         calender.add(labelEvents, 0, 1, 2, 3);
     }
 }
