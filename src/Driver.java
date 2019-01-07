096f7110cefc125a7ab105bf5c65abf7d046d1eb
Ethan Neece <ethan.neece1@gmail.com>
2019-01-07 17:41:34 -0500

    PUSHY

--- src/Driver.java
+++ src/Driver.java
@@ -1,9 +1,11 @@
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -24,52 +26,68 @@ public class Driver extends Application {
         BorderPane root = new BorderPane();
 
         GridPane calender = new GridPane();
-        //calender.setAlignment(Pos.CENTER);
+        calender.setAlignment(Pos.CENTER);
         root.setCenter(calender);
         Day day = new Day(1);
         day.addActivity(new Activity("Hello World", 30, 60));
         day.addActivity(new Activity("What you think?", 70, 120));
-        calender.add(createDay(day), 0, 0);
-        //addDay(primaryStage, calender);
+        calender.add(createDay(day), 10, 10);
 
         primaryStage.setScene(new Scene(root, 1980,1080));
         primaryStage.show();
     }
 
-//    public void addDay(Stage primaryStage, GridPane calender) throws Exception {
-//        ImageView dayDesign = new ImageView(new Image(Driver.class.getResourceAsStream("Day Design.png")));
-//        calender.add(dayDesign, 0, 0, 2, 4);
-//
-//        Label labelDay = new Label(" Day 1");
-//        labelDay.setStyle("-fx-font-size: 16;");
-//        calender.add(labelDay, 0, 0, 1, 1);
-//
-//        Button add = new Button("+");
-//        add.setStyle("-fx-base: #7f9ed1");
-//        calender.add(add, 5, 0, 5, 3);
-//
-//        Label labelEvents = new Label("  -Eat food"); //where to add time?
-//        calender.add(labelEvents, 0, 1, 2, 3);
-//    }
-
     private VBox createDay(Day day) {
-        VBox dayBox = new VBox();
+        VBox dayBox = new VBox(5);
 
+        //Panel to add activities.
+        HBox hiddenPanel = new HBox(20);
+        hiddenPanel.getChildren().add(new Text("Add Activity: "));
+        Button addActivity = new Button("+");
+
+        addActivity.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                GridPane input = new GridPane();
+                input.setHgap(15);
+                input.setVgap(10);
+
+                Text activityTitle = new Text("New Activity");
+                Text[] define = { new Text("Activity: "), new Text("Start Time: "), new Text("End Time")} ;
+                TextField[] inputers = { new TextField(), new TextField(), new TextField() };
+                input.add(activityTitle, 0, 0);
+
+                for (int i = 1; i < define.length + 1; i++) {
+                    input.add(define[i - 1], i, 0);
+                    input.add(inputers[i - 1], i, i);
+                }
+                dayBox.getChildren().add(input);
+            }
+        });
+
+        hiddenPanel.getChildren().add(addActivity);
+        hiddenPanel.setVisible(false);
+        dayBox.getChildren().add(hiddenPanel);
+
+
+
+        //Setting up functionality of disappearing and reappearing button.
+        dayBox.setOnMouseEntered(e -> hiddenPanel.setVisible(true));
+        dayBox.setOnMouseExited(e -> hiddenPanel.setVisible(false));
+
+        //Background
         Image background = new Image("Day Design.png");
         dayBox.setPrefSize(background.getWidth(), background.getHeight());
         dayBox.setBackground(new Background(new BackgroundImage(background, null, null, null, null)));
 
+        //Title of the Day
         Text title = new Text("\n  " + day.getName() + "\t\t\t\t" + day.getDayNumber());
-        //title.setFont();
         dayBox.getChildren().add(title);
 
-        Button add = new Button("+");
-        add.setStyle("-fx-background-color: transparent");
-        add.setOnMouseEntered(e -> add.setStyle("-fx-base: #7f9ed1"));
-        add.setOnMouseExited(e -> add.setStyle("-fx-background-color: transparent"));
-        dayBox.getChildren().add(add);
+        //Adding activities in a list.
         Text activities = new Text(day.getStringActivities());
         dayBox.getChildren().add(activities);
+
         return dayBox;
     }
 }
